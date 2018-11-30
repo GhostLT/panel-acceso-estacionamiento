dcfba10850b6460286fd65b3320471c7ddb267ee
Javier <viverosjavier86@gmail.com>
2018-11-29 18:36:15 -0600

    panel de acceso de estacionamiento V1.0 compartido

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<section class="login-block">
+	<div class="container">
+		<div class="row mt-5">
+			<div class="col-md-4 login-sec mt-5">
+				<h2 class="text-center">Acceso a Estacionamiento</h2>
+				<form class="login-form">
+					<div class="form-group">
+						<label for="id" class="text-uppercase">Id</label>
+						<input type="text" class="form-control" placeholder="">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputEmail1" class="text-uppercase">Nombre</label>
+						<input type="text" class="form-control" placeholder="">
+					</div>
+					<div class="form-group">
+						<label for="placas" class="text-uppercase">Placas</label>
+						<input type="text" class="form-control" placeholder="">
+					</div>
+					
+					
+					<!--<div class="form-check">-->
+					<!--<label class="form-check-label">-->
+					<!--<input type="checkbox" class="form-check-input">-->
+					<!--<small>Remember Me</small>-->
+					<!--</label>-->
+					<!--<button type="submit" class="btn btn-login float-right">Submit</button>-->
+					<!--</div>-->
+				
+				</form>
+				<div class="copy-text">Creado con <i class="fa fa-heart"></i> por el <a href="http://grafreez.com">Nodo de Creatividad</a></div>
+			</div>
+			<div class="col-md-8 banner-sec mt-4">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner" role="listbox">
+						<div class="carousel-item active">
+							<img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+							<div class="carousel-caption d-none d-md-block">
+								<div class="banner-text">
+									<h2>Comunicado 1</h2>
+									<p>Acceso solo a Personal Autorizado</p>
+								</div>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
+							<div class="carousel-caption d-none d-md-block">
+								<div class="banner-text">
+									<h2>Comunicado 2</h2>
+									<p>Para darte de alta manda un correo a nododecreatividad@gmail.com</p>
+								</div>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
+							<div class="carousel-caption d-none d-md-block">
+								<div class="banner-text">
+									<h2>Comunicado 3</h2>
+									<p>Si tu tarjeta se daño tiene un valor de $10.00 Pesos por costo de reposición</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+		</div>
+</section>
+<script src="jQuery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
